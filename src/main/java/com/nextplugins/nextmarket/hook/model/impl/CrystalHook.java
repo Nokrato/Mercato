b6211d5c7352ef40a218b2a1f842ba655c12b5c7
Yuhtin <rio.daviduarte@gmail.com>
2021-04-10 05:29:24 -0300

    impl(hook): crystal & tokens hook

---
+++ src/main/java/com/nextplugins/nextmarket/hook/model/impl/CrystalHook.java
@@ -0,0 +1,46 @@
+package com.nextplugins.nextmarket.hook.model.impl;
+
+import com.nextplugins.nextmarket.hook.model.CurrencyHook;
+import com.yuhtin.minecraft.heroesmines.service.player.MinePlayer;
+import com.yuhtin.minecraft.heroesmines.service.player.controller.PlayerController;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.OfflinePlayer;
+
+/**
+ * @author Yuhtin
+ * Github: https://github.com/Yuhtin
+ */
+public class CrystalHook implements CurrencyHook {
+
+    @Override
+    public boolean has(OfflinePlayer player, double amount) {
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        return minePlayer.getCrystals() >= amount;
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player) {
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        return minePlayer.getCrystals();
+    }
+
+    @Override
+    public void depositCoins(OfflinePlayer player, double amount) {
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        minePlayer.addCrystals(amount);
+    }
+
+    @Override
+    public EconomyResponse withdrawCoins(OfflinePlayer player, double amount) {
+
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        if (minePlayer.getCrystals() < amount) {
+            return new EconomyResponse(amount, minePlayer.getCrystals(), EconomyResponse.ResponseType.FAILURE, "");
+        }
+        
+        minePlayer.setCrystals(minePlayer.getCrystals() - amount);
+        return new EconomyResponse(amount, minePlayer.getCrystals(), EconomyResponse.ResponseType.SUCCESS, "");
+
+    }
+
+}
---
+++ src/main/java/com/nextplugins/nextmarket/hook/model/impl/TokensHook.java
@@ -0,0 +1,46 @@
+package com.nextplugins.nextmarket.hook.model.impl;
+
+import com.nextplugins.nextmarket.hook.model.CurrencyHook;
+import com.yuhtin.minecraft.heroesmines.service.player.MinePlayer;
+import com.yuhtin.minecraft.heroesmines.service.player.controller.PlayerController;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.OfflinePlayer;
+
+/**
+ * @author Yuhtin
+ * Github: https://github.com/Yuhtin
+ */
+public class TokensHook implements CurrencyHook {
+
+    @Override
+    public boolean has(OfflinePlayer player, double amount) {
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        return minePlayer.getTokens() >= amount;
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player) {
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        return minePlayer.getTokens();
+    }
+
+    @Override
+    public void depositCoins(OfflinePlayer player, double amount) {
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        minePlayer.addTokens(amount);
+    }
+
+    @Override
+    public EconomyResponse withdrawCoins(OfflinePlayer player, double amount) {
+
+        MinePlayer minePlayer = PlayerController.getByPlayer(player.getName());
+        if (minePlayer.getTokens() < amount) {
+            return new EconomyResponse(amount, minePlayer.getTokens(), EconomyResponse.ResponseType.FAILURE, "");
+        }
+
+        minePlayer.setTokens(minePlayer.getTokens() - amount);
+        return new EconomyResponse(amount, minePlayer.getTokens(), EconomyResponse.ResponseType.SUCCESS, "");
+
+    }
+
+}
